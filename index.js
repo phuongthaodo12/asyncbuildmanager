ea6734132656188c0c5e7b3e9748aaae2d26af0c
phuongthaodo12 <lolman.popnam1311@outlook.com>
2024-05-07 23:25:46 +0700

    update aptos task

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-function isSymmetric(root) {
-  if (!root) return true;
-  return isMirror(root.left, root.right);
-  function isMirror(left, right) {
-    if (!left && !right) return true;
-    if (!left || !right || left.val !== right.val) return false;
-    return isMirror(left.left, right.right) && isMirror(left.right, right.left);
-  }
+function isPalindrome(str) {
+  return str === str.split("").reverse().join("");
 }
